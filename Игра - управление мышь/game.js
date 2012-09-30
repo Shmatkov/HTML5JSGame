9e5eb64ca9e398ee853fe7ecef4937e72ef45b63
shmatkov.d.i <shmatkov.d.i@gmail.com>
2012-09-30 21:26:41 +0300

    Игра с управлением мыши
    управление осуществляется только влево и вправо
    анимация перса не доработана

---
+++ Игра - управление мышь/game.js
@@ -0,0 +1,117 @@
+var width = 800, 
+	height = 500,
+	gLoop,
+	c = document.getElementById('c'), 
+	ctx = c.getContext('2d');
+			
+	c.width = width;
+	c.height = height;
+
+
+var clear = function(){
+	ctx.fillStyle = '#0aa10a';
+	ctx.clearRect(0, 0, width, height);
+	ctx.beginPath();
+	ctx.rect(0, 0, width, height);
+	ctx.closePath();
+	ctx.fill();
+}
+
+
+
+var player = new (function(){
+	var that = this;
+	that.image = new Image();
+
+	that.image.src = "p.svg"
+	that.width = 30;
+	that.height = 35;
+	that.frames = 1;
+	that.actualFrame = 0;
+	that.X = 0;
+	that.Y = 0;	
+	
+	/*
+	 * Part 3 - jumping
+	 */
+	
+   
+	
+	
+	
+	
+	
+	
+	
+	that.moveLeft = function(){
+		if (that.X > 0) {
+			that.setPosition(that.X - 5, that.Y);
+		}
+	}
+	
+	that.moveRight = function(){
+		if (that.X + that.width < width) {
+			that.setPosition(that.X + 5, that.Y);
+		}
+	}
+
+	/*
+	 * end of part 3
+	 */
+	
+	that.setPosition = function(x, y){
+		that.X = x;
+		that.Y = y;
+	}
+	
+	that.interval = 0;
+	that.draw = function(){
+		try {
+			ctx.drawImage(that.image, 0, that.height * that.actualFrame, that.width, that.height, that.X, that.Y, that.width, that.height);
+		} 
+		catch (e) {
+		};
+		
+		if (that.interval == 4 ) {
+			if (that.actualFrame == that.frames) {
+				that.actualFrame = 0;
+			}
+			else {
+				that.actualFrame++;
+			}
+			that.interval = 0;
+		}
+		that.interval++;		
+	}
+})();
+
+
+player.setPosition(~~((width-player.width)/2), ~~((height - player.height)/2));
+
+
+document.onmousemove = function(e){
+	if (player.X + c.offsetLeft > e.pageX) {
+		player.moveLeft();
+	} else if (player.X + c.offsetLeft < e.pageX) {
+		player.moveRight();
+	}
+	
+}
+	
+var GameLoop = function(){
+	clear();
+	
+	/*
+	 * part 3
+	 */
+	
+		if (player.isFalling) 
+			player.checkFall();
+    /*
+     * end of part 3
+     */
+	player.draw();
+	gLoop = setTimeout(GameLoop, 1000 / 50);
+}
+
+GameLoop();
